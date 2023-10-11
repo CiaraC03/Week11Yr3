a13e19962e890415e3fae67c24ebf2e3574d5f7a
G00402357@atu.ie <G00402357@atu.ie>
2023-10-11 14:49:05 +0100

    added lombok getters and setters in UserDetails. Created new class called UserController.

--- src/main/java/ie/atu/userservicewk4/UserDetails.java
+++ src/main/java/ie/atu/userservicewk4/UserDetails.java
@@ -1,4 +1,13 @@
 package ie.atu.userservicewk4;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetails {
+    private String name;
+    private String email;
 }
